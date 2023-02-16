451ad99d39ad7454a5963519def5a0ab89f30499
Thao Le <thaole26601@gmail.com>
2023-02-16 13:26:34 +0100

    Update main.r

--- main.r
+++ main.r
@@ -3,8 +3,8 @@
 #------------------------------------
 # load packages
 if(!require(pacman)){install.packages("pacman")}
-p_load(tidyverse, PMA, rlist, latex2exp, knitr, formatR, devtools, reshape2, 
-       ggplot2, ggbiplot, factoextra, pracma, gtools,sparsepca, ggplot2)
+p_load(tidyverse, PMA, latex2exp, knitr, formatR, devtools, 
+       ggplot2, ClusterR, cluster, factoextra, gtools, ggplot2, caret, corrplot)
 
 # set seed
 set.seed(432)
@@ -31,16 +31,15 @@ dfData[, -c(1)] <- scale(dfData[, -c(1)]) %>% as.matrix()
 #----------------------------------------------
 #         Principle Component Analysis            
 #----------------------------------------------
-lPCA = prcomp(dfData, center = TRUE, scale = TRUE)
+lPCA = prcomp(dfData[, -c(1)], center = TRUE, scale = TRUE)
 
 # Choose PC with eigenvalues of greater than 1
-library(factoextra)
 eig.val <- get_eigenvalue(lPCA)
 eig.val
 
 #Scree plot 
-fviz_eig(lPCA, addlabels = TRUE, 
-         linecolor = "Red", ylim = c(0, 50))
+fviz_eig(lPCA, addlabels = TRUE, barcolor = "deepskyblue",  barfill = "deepskyblue",
+         linecolor = "hotpink", ylim = c(0, 50))
 # Variable plot
 fviz_pca_var(lPCA,
              col.var = "contrib", # Color by contributions to the PC
@@ -58,16 +57,15 @@ plot(
 )
 
 # Correlation plot
-library(corrplot)
 var <- get_pca_var(lPCA)
 var$cor[,1:6]
-corrplot(var$cor[,1:6], is.corr=FALSE)
+corrplot(var$cor[,1:2], is.corr=FALSE)
 
-# Total cos2 of variables on Dim.1 and Dim.3
-fviz_cos2(lPCA, choice = "var", axes = 1:3 , top = 15)
+# Total cos2 of variables on Dim.1 and Dim.2 QUESTION: choose first 3 or 6?
+fviz_cos2(lPCA, choice = "var", axes = 1:2 , top = 10)
 
 # Contributions of variables to PC1
-fviz_contrib(lPCA, choice = "var", axes = 1, top = 10)
+fviz_contrib(lPCA, choice = "var", axes = 1:2, top = 30)
 # Contributions of variables to PC2
 fviz_contrib(lPCA, choice = "var", axes = 2, top = 10)
 
@@ -76,14 +74,42 @@ fviz_pca_ind(lPCA,
              repel = TRUE, # Avoid text overlapping (slow if many points)
              geom.ind = "point", # show points only (nbut not "text")
              col.ind = factor(dfData$diagnosis), # color by groups
-             palette =  c("#00AFBB", "FC4E07"),
-             addEllipses = TRUE, # Concentration ellipses
+             palette =  c("#00AFBB", "#FC4E07"),
+            # addEllipses = TRUE, # Concentration ellipses
              legend.title = "Groups"
 )
 
 # plot diagnosis scatter
 Diagnosis = dfData$diagnosis
 PC_plot<-ggplot(as.data.frame(lPCA$x), aes(x=PC1, y=PC2,group=Diagnosis, col= Diagnosis))+ 
-  geom_point(alpha=0.5) + #stat_ellipse(aes(color=Diagnosis), level = 0.95)
+  geom_point(alpha=0.5) + stat_ellipse(aes(color=Diagnosis), level = 0.95)
 PC_plot + scale_color_gradient(low="deepskyblue", high="hotpink")
+
+
+#----------------------------------------------
+#             K-mean clustering            
+#----------------------------------------------
+
+# Get principle components
+dfPCA = lPCA$x[,1:2]
+
+# CLustering
+kmeans <- kmeans(dfPCA, centers = 2, nstart = 20)
+kmeans$cluster <-replace(kmeans$cluster, kmeans$cluster==2, 0)
+kmeans$cluster
+
+# Confusion Matrix
+confusionMatrix(factor(kmeans$cluster), factor(dfData$diagnosis), mode = "prec_recall", positive="1")
+
+# Model Evaluation and visualization
+# plot diagnosis scatter from kmean
+require(gridExtra)
+K_mean_diagnosis = kmeans$cluster
+PC_plot1<-ggplot(as.data.frame(lPCA$x), aes(x=PC1, y=PC2,group=K_mean_diagnosis, col= K_mean_diagnosis))+ 
+  geom_point(alpha=0.5) + stat_ellipse(aes(color=K_mean_diagnosis), level = 0.90) + scale_color_gradient(low="deepskyblue", high="hotpink")
+# plot diagnosis scatter from kmean
+Real_diagnosis = dfData$diagnosis
+PC_plot2<-ggplot(as.data.frame(lPCA$x), aes(x=PC1, y=PC2,group=Real_diagnosis, col= Real_diagnosis))+ 
+  geom_point(alpha=0.5) + stat_ellipse(aes(color=Real_diagnosis), level = 0.90)+ scale_color_gradient(low="deepskyblue", high="hotpink")
+grid.arrange(PC_plot1, PC_plot2, ncol=2)
 
